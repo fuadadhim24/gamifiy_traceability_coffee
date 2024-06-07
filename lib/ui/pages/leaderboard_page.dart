c1303420aebaa7a63801129c9dbb84ef2b43a354
fuadadhim24 <fuadadhim24@gmail.com>
2024-06-07 09:49:30 +0700

    feat: add sliding up panel at leaderboard page

--- lib/ui/pages/leaderboard_page.dart
+++ lib/ui/pages/leaderboard_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gamify_traceability_coffee/shared/theme.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class LeaderboardPage extends StatefulWidget {
   const LeaderboardPage({super.key});
@@ -15,8 +17,42 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
         toolbarHeight: 88,
         centerTitle: true,
         title: Text(
-          'Leaderboard'
+          'Leaderboard',
+          style: BlackTextStyle.copyWith(
+            fontWeight: semiBold,
+            fontSize: 20,
+          ),
         ),
+      ),
+      body: SlidingUpPanel(
+        margin: EdgeInsets.symmetric(horizontal: 8),
+        color: greyBackgroundColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+        maxHeight: MediaQuery.of(context).size.height / 4,
+        minHeight: MediaQuery.of(context).size.height,
+        panel: Container(
+          child: buildSlidingUpPanel(),
+        ),
+        body: buildBodyPage(),
+      ),
+    );
+  }
+
+  Widget buildSlidingUpPanel() {
+    return Container(
+      child: Center(
+        child: Text('Sliding Up Panel'),
+      ),
+    );
+  }
+
+  Widget buildBodyPage() {
+    return Container(
+      child: Center(
+        child: Text('Body Page'),
       ),
     );
   }
