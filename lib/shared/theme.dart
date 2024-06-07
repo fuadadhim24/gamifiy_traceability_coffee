a36900f78b8720104c95bf15fe34e84a3e45b3a5
fuadadhim24 <fuadadhim24@gmail.com>
2024-06-07 08:59:47 +0700

    flutter init

---
+++ lib/shared/theme.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+Color greenBackgroundColor = const Color(0xff38A3A5);
+Color greyBackgroundColor = const Color(0xffEFEEFC);
+Color whiteBackgroundColor = const Color(0xffFFFFFF);
+
+TextStyle whiteTextStyle = 
---
+++ lib/ui/pages/leaderboard_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class LeaderboardPage extends StatefulWidget {
+  const LeaderboardPage({super.key});
+
+  @override
+  State<LeaderboardPage> createState() => _LeaderboardPageState();
+}
+
+class _LeaderboardPageState extends State<LeaderboardPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 88,
+        centerTitle: true,
+        title: Text(
+          'Leaderboard'
+        ),
+      ),
+    );
+  }
+}
