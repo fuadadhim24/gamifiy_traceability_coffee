337e1118e050896c33bfc243412796bf0d773bf6
fuadadhim24 <fuadadhim24@gmail.com>
2024-06-07 09:10:51 +0700

    feat: add theme

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,7 +1,28 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 Color greenBackgroundColor = const Color(0xff38A3A5);
 Color greyBackgroundColor = const Color(0xffEFEEFC);
 Color whiteBackgroundColor = const Color(0xffFFFFFF);
+Color txtBlackColor = const Color(0xff0C092A);
+Color txtGreyColor = const Color(0xff858494);
 
-TextStyle whiteTextStyle = 
+TextStyle WhiteTextStyle = GoogleFonts.inter(
+  color: whiteBackgroundColor,
+);
+
+TextStyle GreenTextStyle = GoogleFonts.inter(
+  color: greenBackgroundColor,
+);
+
+TextStyle BlackTextStyle = GoogleFonts.inter(
+  color: txtBlackColor,
+);
+
+TextStyle GreyTextStyle = GoogleFonts.inter(
+  color: txtGreyColor,
+);
+
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
