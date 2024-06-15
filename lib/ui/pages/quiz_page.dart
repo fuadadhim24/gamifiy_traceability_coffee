42546a1c9cb1382e18f62c18224b6fbf60b3baa8
nmuhammadfadli <fadligans98@gmail.com>
2024-06-15 09:09:08 +0700

    add result page and revised quiz page

--- lib/ui/pages/quiz_page.dart
+++ lib/ui/pages/quiz_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:gamify_traceability_coffee/models/quiz.dart';
+import 'result_page.dart';
 
 class QuizPage extends StatefulWidget {
   final Quiz quiz;
+  final String username;
 
-  const QuizPage({required this.quiz, super.key});
+  const QuizPage({required this.quiz, required this.username, super.key});
 
   @override
   State<QuizPage> createState() => _QuizPageState();
@@ -29,34 +31,21 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   void _showResult() {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: const Text('Hasil Kuis'),
-        content: Text('Skor Anda: $_score'),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-              setState(() {
-                _currentQuestionIndex = 0;
-                _score = 0;
-              });
-            },
-            child: const Text('Ulangi',
-             style: TextStyle(color: Colors.green, fontSize: 14),),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pop();
-              Navigator.of(context).pop(); 
-            },
-            child: const Text('Kembali ke Daftar Kuis',
-            style: TextStyle(color: Colors.green, fontSize: 14),),
-          ),
-        ],
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => ResultPage(
+          score: _score,
+          username: widget.username,
+          quizTitle: widget.quiz.title,
+        ),
       ),
-    );
+    ).then((_) {
+      setState(() {
+        _currentQuestionIndex = 0;
+        _score = 0;
+      });
+    });
   }
 
   @override
@@ -65,7 +54,9 @@ class _QuizPageState extends State<QuizPage> {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.quiz.title,style: TextStyle(color: Colors.green, fontSize: 14),),
+        title: Text(widget.quiz.title, style: const TextStyle(color: Colors.white)),
+        backgroundColor: Colors.green,
+          automaticallyImplyLeading: false,
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -74,7 +65,7 @@ class _QuizPageState extends State<QuizPage> {
           children: [
             Text(
               question.content,
-              style: const TextStyle(color: Colors.green,fontSize: 24.0, fontWeight: FontWeight.bold),
+              style: const TextStyle(color: Colors.green, fontSize: 24.0, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 20.0),
             ...question.options.map((option) => Container(
@@ -82,6 +73,7 @@ class _QuizPageState extends State<QuizPage> {
                   child: ElevatedButton(
                     onPressed: () => _nextQuestion(option.isCorrect),
                     style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
                       padding: const EdgeInsets.all(16.0),
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(12.0),
@@ -89,7 +81,7 @@ class _QuizPageState extends State<QuizPage> {
                     ),
                     child: Text(
                       option.content,
-                      style: const TextStyle(fontSize: 18.0),
+                      style: const TextStyle(color: Colors.white,fontSize: 18.0),
                     ),
                   ),
                 )),
